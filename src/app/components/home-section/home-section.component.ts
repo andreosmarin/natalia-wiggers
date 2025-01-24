5d32224c8ba6cb5be65b9f8c5a6bf2879835971e
André Osmarin <andre.osmarin@gmail.com>
2025-01-24 00:26:26 -0300

    feat: adjusts in home section

--- src/app/components/home-section/home-section.component.ts
+++ src/app/components/home-section/home-section.component.ts
@@ -1,12 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatButton } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-section',
-  imports: [NgOptimizedImage, MatGridListModule, MatButton, RouterLink],
+  imports: [NgOptimizedImage, MatGridListModule, RouterLink],
   templateUrl: './home-section.component.html',
   styleUrl: './home-section.component.scss',
 })
