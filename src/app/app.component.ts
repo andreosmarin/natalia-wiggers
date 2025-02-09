b98269d39535f95ae9bbc1cad802f5d99cd2e744
André Osmarin <andre.osmarin@gmail.com>
2025-02-09 19:38:31 -0300

    feat: add meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 import { AboutSectionComponent } from './components/about-section/about-section.component';
@@ -27,8 +27,36 @@ import { TestimonialsSectionComponent } from './components/testimonials-section/
 })
 export class AppComponent {
   title = $localize`Natália Wiggers | Image & Style Consulting`;
+  description = $localize`Graduated in Fashion Design and specialized in Image & Style Consulting, I help women connect with their essence and express a unique style.`;
 
-  constructor(private titleService: Title) {
+  constructor(
+    private titleService: Title,
+    private meta: Meta,
+  ) {
     this.titleService.setTitle(this.title);
+    this.meta.addTag({
+      name: 'og:title',
+      content: this.title,
+    });
+    this.meta.addTag({
+      name: 'name',
+      content: this.title,
+    });
+    this.meta.addTag({
+      name: 'description',
+      content: this.description,
+    });
+    this.meta.addTag({
+      name: 'og:description',
+      content: this.description,
+    });
+    this.meta.addTag({
+      name: 'og:site_name',
+      content: 'Natália Wiggers',
+    });
+    this.meta.addTag({
+      name: 'og:image',
+      content: 'public/image-logo-natalia-wiggers.png',
+    });
   }
 }
