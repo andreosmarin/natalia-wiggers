7ea88ab309535b7f1ce8a7330774f31e3beeca75
André Osmarin <andre.osmarin@gmail.com>
2025-02-10 00:12:14 -0300

    fix: adjust internalization

--- src/app/shared/system/languages.ts
+++ src/app/shared/system/languages.ts
@@ -1,7 +1,7 @@
 import { Language } from './language.interface';
 
 export const Languages: Language[] = [
-  { label: 'PT', id: 'pt-BR', flagSvg: 'brazilian-flag.svg' },
-  { label: 'EN', id: 'en-US', flagSvg: 'usa-flag.svg' },
-  { label: 'ES', id: 'es-US', flagSvg: 'flag-of-spain.svg' },
+  { label: 'PT', id: 'pt', flagSvg: 'brazilian-flag.svg' },
+  { label: 'EN', id: 'en', flagSvg: 'usa-flag.svg' },
+  { label: 'ES', id: 'es', flagSvg: 'flag-of-spain.svg' },
 ];
