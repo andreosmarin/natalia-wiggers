dce83667a57a199ad7afdbf3d839d575036e1320
André Osmarin <andre.osmarin@gmail.com>
2025-02-09 21:57:47 -0300

    feat: add new services

--- src/app/components/services-section/services-section.component.ts
+++ src/app/components/services-section/services-section.component.ts
@@ -13,13 +13,13 @@ export class ServicesSectionComponent {
   services: CarouselItem[] = [
     {
       title: $localize`Personal Coloring`,
-      description: $localize`Discover the colors that enhance your natural beauty and elevate your image.`,
+      description: $localize`Discover the colors that best harmonize with your features. Colors can say a lot about you!`,
       image: 'service-personal-coloring.jpg',
       active: true,
     },
     {
       title: $localize`Closet Detox`,
-      description: $localize`Refresh your wardrobe by removing pieces that don't match your style and highlighting those that truly make sense for you.`,
+      description: $localize`Make dressing easier by strategically removing pieces from your wardrobe that go against your desired image.`,
       image: 'service-closet-detox.jpg',
       active: false,
     },
@@ -27,6 +27,18 @@ export class ServicesSectionComponent {
       title: $localize`Personal Shopper`,
       description: $localize`Enjoy a personalized shopping experience with choices that reflect your style, needs, and goals.`,
       image: 'service-personal-shopper.jpg',
+      active: false,
+    },
+    {
+      title: $localize`Outfit Assembly`,
+      description: $localize`Create authentic and versatile outfit combinations that reflect your personality and style!`,
+      image: 'service-outfit-assembly.png',
+      active: false,
+    },
+    {
+      title: $localize`Smart Suitcase`,
+      description: $localize`Pack smart and travel light! Learn to choose key pieces, create stylish outfits, and stay organized with ease.`,
+      image: 'service-smart-suitcase.jpg',
       active: false,
     },
   ];
